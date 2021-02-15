7ca66bd974ade739a79c2b2ce7e38914f149126e
Rainstorm <legrand.adrien96@gmail.com>
2021-02-15 20:10:51 +0100

    fetch data from newsapi + display data and images in recyclerview + swiperefresh added

--- app/src/main/java/com/app/all_my_news/RecAdapter.kt
+++ app/src/main/java/com/app/all_my_news/RecAdapter.kt
@@ -1,12 +1,18 @@
 package com.app.all_my_news
 
+import android.graphics.drawable.Drawable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.app.all_my_news.RecAdapter.RecViewHolder
+import com.squareup.picasso.Picasso
+import java.io.InputStream
+import java.net.URL
+
 
 class RecAdapter(private val list: List<Section>?) : RecyclerView.Adapter<RecViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecViewHolder {
@@ -21,14 +27,12 @@ class RecAdapter(private val list: List<Section>?) : RecyclerView.Adapter<RecVie
         Log.d("DebugTag", "bind")
         var cpt: Int = 0
         for (section in list!!){
-            Log.d("id", "id="+cpt+" "+section.expanded)
             holder.bind(section)
             cpt += 1
         }
         val section = list!![position]
         holder.bind(section)
-        holder.itemView.setOnClickListener {
-                v: View? ->
+        holder.itemView.setOnClickListener { v: View? ->
             section.expanded = !section.expanded
             notifyItemChanged(position)
         }
@@ -40,8 +44,9 @@ class RecAdapter(private val list: List<Section>?) : RecyclerView.Adapter<RecVie
 
     inner class RecViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val title: TextView = itemView.findViewById(R.id.item_title)
-        private val genre: TextView = itemView.findViewById(R.id.sub_item_content)
+        private val description: TextView = itemView.findViewById(R.id.sub_item_description)
         private val subItem: View = itemView.findViewById(R.id.sub_item)
+        private val image: ImageView = itemView.findViewById(R.id.imageView)
         fun bind(section: Section) {
             //val recview : RecyclerView = itemView.rootView.findViewById(R.id.recview)!!
             /*for(section in recview){
@@ -50,8 +55,8 @@ class RecAdapter(private val list: List<Section>?) : RecyclerView.Adapter<RecVie
             //Log.d("Child count",""+ recview.childCount)
             subItem.visibility = if (section.expanded) View.VISIBLE else View.GONE
             title.text = section.title
-            genre.text = section.content
+            description.text = section.content
+            Picasso.get().load(section.imgUrl).into(image)
         }
-
     }
 }
--- app/src/main/java/com/app/all_my_news/Section.kt
+++ app/src/main/java/com/app/all_my_news/Section.kt
@@ -1,3 +1,3 @@
 package com.app.all_my_news
 
-class Section(var title: String, var content: String, var expanded: Boolean)
+class Section(var title: String, var content: String, var imgUrl: String,  var expanded: Boolean)
