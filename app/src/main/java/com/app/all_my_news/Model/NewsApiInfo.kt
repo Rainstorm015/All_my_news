9e30bf43da26a4a9fc21a80a1d504c9a278513f6
Rainstorm <legrand.adrien96@gmail.com>
2021-02-21 14:26:24 +0100

    country variable added in object

--- app/src/main/java/com/app/all_my_news/Model/NewsApiInfo.kt
+++ app/src/main/java/com/app/all_my_news/Model/NewsApiInfo.kt
@@ -3,11 +3,18 @@ package com.app.all_my_news.Model
 class NewsApiInfo {
     var id: Int = 0
     var key: String = ""
+    var country: String = ""
 
     constructor()
 
     constructor(id: Int, key: String){
         this.id = id
         this.key = key
+    }
+
+    constructor(id: Int, key: String, country: String){
+        this.id = id
+        this.key = key
+        this.country = country
     }
 }
